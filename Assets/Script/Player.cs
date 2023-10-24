18da09ffa2dbee730fb5981a39e138ef26e18292
MSI\hidey <hideya.t.e.s.a.s@gmail.com>
2023-10-24 13:39:51 +0900

    スワイプでプレイヤーを操作できるスクリプトを追加

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Vector3 offset;
+    Vector3 offset;
+    public float sensitivity = 1f;
+    const float LOAD_WIDTH = 10f;
+    const float LOAD_HEIGHT = 10f;
+    const float MOVE_MAX_X = 8f;
+    const float MOVE_MAX_Y = 4.3f;
+    Vector3 previousPos, currentPos;
 
-    void OnMouseDown()
+    void Update()
     {
-        offset = gameObject.transform.position - GetMouseWorldPos();
-    }
+        if (Input.GetMouseButtonDown(0))
+        {
+            previousPos = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            // スワイプによる移動距離を取得
+            currentPos = Input.mousePosition;
+            float diffDistanceX = (currentPos.x - previousPos.x) / Screen.width * LOAD_WIDTH;
+            float diffDistanceY = (currentPos.y - previousPos.y) / Screen.width * LOAD_HEIGHT;
+            diffDistanceX *= sensitivity;
+            diffDistanceY *= sensitivity;
 
-    void OnMouseDrag()
-    {
-        transform.position = GetMouseWorldPos() + offset;
+            // 次のローカルx座標を設定 ※道の外にでないように
+            float newX = Mathf.Clamp(transform.localPosition.x + diffDistanceX, -MOVE_MAX_X, MOVE_MAX_X);
+            float newY = Mathf.Clamp(transform.localPosition.y + diffDistanceY, -MOVE_MAX_Y, MOVE_MAX_Y);
+            transform.localPosition = new Vector3(newX, newY, 0);
+
+            // タップ位置を更新
+            previousPos = currentPos;
+        }
     }
 
     private Vector3 GetMouseWorldPos()
