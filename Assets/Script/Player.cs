3e544eac8d1dd319018e0002f05e9c29845b64f8
MSI\hidey <hideya.t.e.s.a.s@gmail.com>
2023-10-24 11:43:53 +0900

    Playerをマウスを動かして移動させるスクリプトを追加

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        offset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + offset;
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
